0761c669f998a67b48d5f18a65ee905c454fb833
Oliver Krížovský <oliver.krizovsky@gmail.com>
2025-04-09 05:00:30 +0200

    základny config, JWT token, login API

---
+++ app.py
@@ -0,0 +1,85 @@
+import os
+import jwt
+import datetime
+import bcrypt
+from functools import wraps
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+import psycopg2
+import requests
+
+# Načítanie credentials z .env súboru
+load_dotenv()
+app = Flask(__name__)
+SECRET_KEY = os.environ.get("SECRET_KEY")
+url = os.environ.get("DATABASE_URL")
+connection = psycopg2.connect(url)
+
+# overenia JWT tokenu
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'Authorization' in request.headers:
+            bearer = request.headers['Authorization']
+            token = bearer.split(" ")[1] if " " in bearer else bearer
+
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+            request.user = data
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token expired'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'message': 'Invalid token'}), 401
+
+        return f(*args, **kwargs)
+    return decorated
+
+# Testovací endpoint
+@app.get("/default")
+@token_required
+def default():
+    user_data = request.user  # získané z tokenu
+    return jsonify({
+        "message": "Access granted",
+        "user_id": user_data['uid'],
+        "role": user_data['role']
+    }), 200
+
+# LOGIN s generovaním JWT tokenu
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT uid, password, role FROM users WHERE email = %s;",
+                (email,)
+            )
+            user = cursor.fetchone()
+
+        if user:
+            uid, hashed_pw, role = user
+
+            if bcrypt.checkpw(password.encode('utf-8'), hashed_pw.encode('utf-8')):
+                token = jwt.encode({
+                    'uid': uid,
+                    'role': role,
+                    'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1)
+                }, SECRET_KEY, algorithm='HS256')
+
+                return jsonify({'success': True, 'token': token}), 200
+            else:
+                return jsonify({'success': False, 'message': 'Invalid password'}), 401
+        else:
+            return jsonify({'success': False, 'message': 'User not found'}), 404
+
+    except Exception as e:
+        print("Error during login:", e)
+        return jsonify({'success': False, 'message': 'Server error'}), 500
